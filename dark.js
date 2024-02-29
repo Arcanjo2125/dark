359825803acc625498d0de818924ddcc69d2d37c
Arcanjo2125 <160789437+Arcanjo2125@users.noreply.github.com>
2024-02-28 21:08:58 -0300

    Add files via upload

---
+++ dark.js
@@ -0,0 +1,23 @@
+function abc() {
+    document.getElementById('active').style.display="none";
+
+    document.getElementById('unactive').style.display="block";
+
+    document.getElementById('body').style.background="whitesmoke";
+
+    document.getElementById('box').style.background="#fff";
+
+    document.getElementById('h4').style.color="steelblue";
+}
+
+function bca() {
+    document.getElementById('unactive').style.display="none";
+
+    document.getElementById('active').style.display="block";
+
+    document.getElementById('body').style.background="#333";
+
+    document.getElementById('box').style.background="#444";
+
+    document.getElementById('h4').style.color="3fa";
+}
